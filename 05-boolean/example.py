006e1e765b881b02859c8204b919c279e67244c8
codio-units <spackham@codio.com>
2015-10-20 15:30:56 +0000

    Added comments to match flode example

--- 05-boolean/example.py
+++ 05-boolean/example.py
@@ -1,11 +1,12 @@
 
-isCold = False
-hasJumper = True
+isCold = False            # Toggle between true and false
 
 if isCold:
   print('Shiver')
 else:
   print('Sweat')
+
+hasJumper = True          # Toggle also between true and false
 
 if not hasJumper:
   print('Buy a jumper')
